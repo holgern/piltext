01e942034331459cbc692437483da3c2be1898d0
holgern <holgern@users.noreply.github.com>
2025-01-29 08:47:13 +0100

    Helper functions

--- piltext/image_drawer.py
+++ piltext/image_drawer.py
@@ -11,6 +11,13 @@ class ImageDrawer:
         self.font_manager = font_manager or FontManager()
         self.draw = ImageDraw.Draw(self.image_handler.image)
 
+    def initialize(self):
+        self.image_handler.initialize()
+
+    def change_size(self, width, height):
+        """Changes the size of the image and re-initializes it."""
+        self.image_handler.change_size(width, height)
+
     def draw_text(self, text, start, end=None, font_name=None, **kwargs):
         """Draw text on the image with optional scaling to fit within a bounding box.
 
