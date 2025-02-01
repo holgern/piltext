212fe63e62e299b2104d801faf8072c27b669cfe
holgern <holgern@users.noreply.github.com>
2025-02-01 11:14:55 +0100

    fix bug in fit_text

--- piltext/text_box.py
+++ piltext/text_box.py
@@ -9,7 +9,12 @@ class TextBox:
         self.font_manager = font_manager
 
     def fit_text(
-        self, draw: ImageDraw, max_width, max_height, font_name=None, start_font_size=1
+        self,
+        draw: ImageDraw,
+        max_width: int,
+        max_height: int,
+        font_name=None,
+        start_font_size: int = 1,
     ):
         """Determine the largest font size that fits within max_width and max_height."""
         font_size = start_font_size
@@ -17,7 +22,10 @@ class TextBox:
         while True:
             width, height = self.font_manager.calculate_text_size(draw, self.text, font)
             if width > max_width or height > max_height:
-                return self.font_manager.build_font(font_name, font_size - 1)
+                font_size -= 1
+                if font_size < 1:
+                    font_size = 1
+                return self.font_manager.build_font(font_name, font_size)
             font_size += 1
             font = self.font_manager.build_font(font_name, font_size)
         return font
--- tests/test_image_drawer.py
+++ tests/test_image_drawer.py
@@ -10,17 +10,25 @@ class TestImageDrawer(unittest.TestCase):
             os.path.join(os.path.dirname(os.path.realpath(__file__)), "fonts")
         ]
         self.font_manager = FontManager(fontdirs=self.fontdirs)
-        self.image_drawer = ImageDrawer(100, 100, font_manager=self.font_manager)
+        self.image_drawer = ImageDrawer(264, 127, font_manager=self.font_manager)
 
     def test_draw_text_with_size_calculation(self):
         # Mock text size calculation and drawing
-        xy = (0, 0)
+        xy = (5, 16)
         w, h, font_size = self.image_drawer.draw_text(
-            "Test", xy, end=(100, 100), font_name="Roboto-Bold"
+            "12345 abcdefg", xy, end=(254, 16.55), font_name="Roboto-Bold", anchor="lt"
         )
-        self.assertEqual(w, 100)
-        self.assertIn(h, [49, 50])
-        self.assertIn(font_size, [51, 52])
+        self.assertEqual(w, 7)
+        self.assertEqual(h, 1)
+        self.assertEqual(font_size, 1)
+        # Mock text size calculation and drawing
+        xy = (5, 16)
+        w, h, font_size = self.image_drawer.draw_text(
+            "12345 abcdefg", xy, end=(254, 25), font_name="Roboto-Bold", anchor="lt"
+        )
+        self.assertEqual(w, 47)
+        self.assertEqual(h, 9)
+        self.assertEqual(font_size, 7)
 
 
 if __name__ == "__main__":
